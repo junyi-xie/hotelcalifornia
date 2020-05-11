c65a5af6bac4e286e0634dd514960062dafc9f58
Junyi Xie <yunyi.xie@outlook.com>
2020-05-11 13:49:47 +0200

    Created navigation concept.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ function scrollToHome() {
   var project = document.getElementById("home");
   project.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToInterior() {
+  var project2 = document.getElementById("interior");
+  project2.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToAmenities() {
+  var project3 = document.getElementById("amenities");
+  project3.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToRooms() {
+  var project4 = document.getElementById("rooms");
+  project4.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToContact() {
+  var project5 = document.getElementById("contact");
+  project5.scrollIntoView({ behavior: "smooth" });
+}
